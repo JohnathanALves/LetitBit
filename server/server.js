800901551a87ca91f2fc3387b0cacc237e47b735
Johnathan <jonata94@gmail.com>
2017-08-17 11:34:27 -0300

    instalacao do jsonFile

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const jsonFile = require('jsonFile');
+var bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
 
 app.listen(port, function () {
   console.log('to ouvindo! hahahah!');
@@ -8,4 +11,9 @@ app.listen(port, function () {
 
 app.get('/', function(req, res){
   res.send('aiaiaiaiai');
+});
+
+app.post('/data', function(req, res){
+  //recebe os dados vindos do esp
+
 });
