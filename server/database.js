fb55d704b2a3d3f15365f98dc605ad7e53a52eaf
leolleocomp <leolleo.comp@gmail.com>
2017-08-29 11:34:58 -0300

    adicao de DB

---
+++ server/database.js
@@ -0,0 +1,10 @@
+//arquivo de configuracao da base de dados Redis
+var redis = require('redis');
+
+module.exports = function (){
+	var client = redis.createClient('12699', 'redis-12699.c14.us-east-1-2.ec2.cloud.redislabs.com'); //creates a new client
+
+	client.on('connect', function() {
+    	console.log('conectado na base de dados com sucesso!');
+	});
+}
